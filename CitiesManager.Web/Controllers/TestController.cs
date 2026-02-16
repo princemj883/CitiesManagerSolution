cc373e5901efe202bc11ffd31d0a2c812231aa80
manoj.jaiswal <mjaiswal@landolakes.com>
2026-02-16 15:23:52 +0530

    added migrations and CitiesController

---
+++ CitiesManager.Web/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public string Method()
+    {
+        return "Hello World!";
+    }
+        
+}
