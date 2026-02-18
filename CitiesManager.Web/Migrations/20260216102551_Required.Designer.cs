b0d47751a3918bc23ccf701f3beb7ab4189f1d33
manoj.jaiswal <mjaiswal@landolakes.com>
2026-02-18 11:59:01 +0530

    content negotiation

--- CitiesManager.Web/Migrations/20260216102551_Required.Designer.cs
+++ CitiesManager.Web/Migrations/20260216102551_Required.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CitiesManager.Web.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CitiesManager.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260216102551_Required")]
+    partial class Required
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CitiesManager.Web/Model/City.cs
+++ CitiesManager.Web/Model/City.cs
@@ -6,5 +6,6 @@ public class City
 {
     [Key]
     public Guid CityId { get; set; }
+    [Required(ErrorMessage = "City name can not be blank")]
     public string? CityName { get; set; }
 }
